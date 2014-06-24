cdab27c6ab09e8d70bfa7030d51eebc4d6a163c4
dotdotcommadot <hans@dotdotcommadot.com>
2014-06-24 13:06:11 +0200

    switches FunctionTypeMirror to ClosureMirror

--- lib/framework/impl/safely_callback.dart
+++ lib/framework/impl/safely_callback.dart
@@ -2,13 +2,13 @@ part of robotlegs;
 
 void safelyCallback(Function callback, [dynamic error = null, dynamic message = null])
 {
-	InstanceMirror mirror = reflect(callback);
+	ClosureMirror mirror = reflect(callback);
 	
-	if ((mirror.type as FunctionTypeMirror).parameters.length == 0)
+	if (mirror.function.parameters.length == 0)
 	{
 		callback();
 	}
-	else if ((mirror.type as FunctionTypeMirror).parameters.length == 1)
+	else if (mirror.function.parameters.length == 1)
 	{
 		callback(error);
 	}
